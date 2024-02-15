cc5c1b144a05a878a762b63f3598b6b5d54ba808
Aman Lama <lama.aman1998@gmail.com>
2024-02-15 23:03:16 +0545

    button styls

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -15,8 +15,23 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Solid: Story = {
     args: {
-        children: "Click Me",
+        children: "Button",
+        variant: "solid",
+    },
+};
+
+export const Outline: Story = {
+    args: {
+        children: "Button",
+        variant: "outline",
+    },
+};
+
+export const Ghost: Story = {
+    args: {
+        children: "Button",
+        variant: "ghost",
     },
 };
